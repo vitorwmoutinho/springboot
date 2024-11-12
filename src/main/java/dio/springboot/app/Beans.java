508b0a8071866ba2c26e8d40055b375857d45e85
vitorwmoutinho <vitor.wenderson@gmail.com>
2024-11-12 14:42:53 -0300

    Criando a classe Beans

--- src/main/java/dio/springboot/app/Beans.java
+++ src/main/java/dio/springboot/app/Beans.java
@@ -1,4 +1,14 @@
 package dio.springboot.app;
 
+import com.google.gson.Gson;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class Beans {
+    @Bean
+    public Gson gson(){
+        return new Gson();
+
+    }
 }
